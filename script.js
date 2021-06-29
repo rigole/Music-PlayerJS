35196930a611bb80a1859c7ce1cdfdb5e6536296
rigole <foplacide@gmail.com>
2021-06-29 17:12:30 +0100

    creating the play and pause functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+let isPlaying = false;
+
+// Play
+function playSong() {
+    isPlaying = true;
+    music.play();
+}
+
+
+// Pause
+function pauseSong() {
+    isPlaying = false;
+    music.pause();
+}
