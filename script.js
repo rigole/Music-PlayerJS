f8d5ba13d87b4a1482b19d5829617443f1b28a91
rigole <foplacide@gmail.com>
2021-06-30 01:21:25 +0100

    Play icon event listener

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let isPlaying = false;
 // Play
 function playSong() {
     isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title','Pause');
     music.play();
 }
 
@@ -15,6 +17,8 @@ function playSong() {
 // Pause
 function pauseSong() {
     isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
 }
 
